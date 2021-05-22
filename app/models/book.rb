f1bcbd4db14a8e8ab2df86675a41d04ad42cb859
Lex-KZ <azaskalkina@hotmail.com>
2021-05-22 10:24:23 +1000

    Add Books validations, relationship to user and the author full name method.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,16 @@
 class Book < ApplicationRecord
+    validates :tittle, presence: true
+    validates :author_first_name, presence: true
+    validates :author_last_name, presence: true
+    validates :pub_year, presence: true, numericality: { only_integer: true }
+    validates :blurb, presence: true
+    validates :physical_description, presence: true
+
+    belongs_to :user
+
+    def author_full_name
+        [ self.author_first_name, self.author_last_name ].join(' ')
+    end
 
 
-    
 end
