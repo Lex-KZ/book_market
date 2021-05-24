d22b007df254734db6654ea30bb5309ab104b0df
Lex-KZ <azaskalkina@hotmail.com>
2021-05-24 12:10:48 +1000

    Add transaction and user relationships and scope.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,11 @@
 class Transaction < ApplicationRecord
-  belongs_to :seller
-  belongs_to :buyer
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
   belongs_to :book
 
   def price
     transaction.cost = book.cost
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ApplicationRecord
 
   has_many :books
 
+  has_many :sales, class_name: "Transaction", foreign_key: :seller_id
+  has_many :sold_books, through: :sales, source: :book
+
+  has_many :purchases, class_name: "Transaction", foreign_key: :buyer_id
+  has_many :purchased_books, through: :purchases, source: :book
+
+  scope :sellers, -> { joins(:sales) }
+  scope :buyers, -> { joins(:purchases) }
+  
   
 end
