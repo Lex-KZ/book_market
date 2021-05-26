3815178ce6826c965a2ca05dea69b7b6c420bf1e
Lex-KZ <azaskalkina@hotmail.com>
2021-05-26 16:48:20 +1000

    Finish stripe integration

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,25 +1,36 @@
 class TransactionsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+
+    before_action :set_book, only: %i[create]
+    
     def create
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
             line_items: [{
             price_data: {
-                unit_amount: 2000,
-                currency: 'usd',
+                unit_amount: (@book.cost * 100),
+                currency: 'aud',
                 product_data: {
-                    name: 'Stubborn Attachments'
+                    name: @book.title
                 },
             },
             quantity: 1,
             }],
             mode: 'payment',
-            success_url: checkout_success_path,
-            cancel_url: books_path,
+            success_url: checkout_success_url,
+            cancel_url: books_url,
         })
-    end
-
-    def success
         
+    
+        render json: { id: session.id }
     end
 
+    def success  
+    end
+
+
+    private
+        def set_book
+            @book = Book.find(params[:id])
+        end
 end
